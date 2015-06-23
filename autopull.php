a1bc2c727dfa0ecf904a27d71cecd714e6b5141c
Jeff Schnepple <schnepple@gmail.com>
2015-06-23 00:45:40 -0700

    autopull script change

--- autopull.php
+++ autopull.php
@@ -20,23 +20,15 @@ $LOCAL_REPO_NAME    = ".";
 $LOCAL_REPO         = "{$LOCAL_ROOT}/{$LOCAL_REPO_NAME}";
 $REMOTE_REPO        = "http://github.com/jschnepple/zettablog";
 $BRANCH             = "master";
-
+print("entering script");
 if ( $_POST['payload'] ) {
   // Only respond to POST requests from Github
-
-  if( file_exists($LOCAL_REPO) ) {
+  print("there is a payload");
 
     // If there is already a repo, just run a git pull to grab the latest changes
-    shell_exec("cd {$LOCAL_REPO} && git pull");
-
+    shell_exec("git pull");
+    //shell_exec("cd {$LOCAL_REPO} && git pull");
     die("done " . mktime());
-  } else {
-
-    // If the repo does not exist, then clone it into the parent directory
-    shell_exec("cd {$LOCAL_ROOT} && git clone {$REMOTE_REPO}");
-
-    die("done " . mktime());
-  }
 }
 
 ?>
