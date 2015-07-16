dc0313e27d0dab697be04adc162b23f367862224
Jeff Schnepple <schnepple@gmail.com>
2015-07-16 02:44:37 -0700

    favicon

--- wp-content/themes/zetta-new/functions.php
+++ wp-content/themes/zetta-new/functions.php
@@ -3,7 +3,16 @@
  * @package WordPress
  * @subpackage Default_Theme
  */
- 
+ // First, create a function that includes the path to your favicon
+function add_favicon() {
+  	$favicon_url = '/admin-favicon.ico';
+	echo '<link rel="shortcut icon" href="' . $favicon_url . '" />';
+}
+  
+// Now, just make sure that function runs when you're on the login page and admin pages  
+add_action('login_head', 'add_favicon');
+add_action('admin_head', 'add_favicon');
+
  add_filter('show_admin_bar', '__return_false');  
 if ( function_exists('register_sidebar') )
     register_sidebar(array(
